3fc20ac0af1703d02dab44323ba4bb6bc0d04eee
Annamariapoli <annamaria402.guerrina@gmail.com>
2016-12-14 10:02:01 +0100

    prog java

---
+++ src/provaJava/Main.java
@@ -0,0 +1,31 @@
+package provaJava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Model m = new Model();		
+		String s1 = "casa";
+		String s2 = "home";
+		String s3 = "light";
+		String s4 = "abaco";
+		String s5 = "prova";
+		String s6 = "vapore";
+		String s7 = "luppolo";
+		String s8 = "fair";		
+	
+		//m.addParola(s1);
+		//m.addParola(s2);
+//		m.addParola(s3);
+//		m.addParola(s4);
+//		m.addParola(s5);
+//		m.addParola(s6);
+//		m.addParola(s7);
+		
+		m.stampaParole();	
+		 m.parolaNonLegata(s8);	
+		//m.totale();
+	   //	m.parolaAccettata(s8);
+
+	}
+
+}
---
+++ src/provaJava/Model.java
@@ -0,0 +1,70 @@
+package provaJava;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Model {
+	
+	private List<String > parole = new LinkedList <String>();
+	
+	public void addParola(String parolaNuova){
+		if(!parole.contains(parolaNuova))                    
+			parole.add(parolaNuova);
+	}
+	
+	public boolean parolaPresente (String parolaNuova){
+		if(parole.contains(parolaNuova)){
+			return true;
+		}
+		else return false;
+	}
+	
+	public boolean parolaAccettata(String parolaNuova){               //se la lista è vuota, la prima parola deve essere accettataa
+	
+		boolean presente = parolaPresente(parolaNuova);
+		boolean nonLegata = parolaNonLegata(parolaNuova);
+		if(!presente && !nonLegata ){
+			parole.add(parolaNuova);
+			System.out.println(true);
+		    return true;
+		}
+		else 
+			System.out.println(false);
+		    return false;	
+	}
+	
+	public boolean parolaNonLegata(String parolaNuova){    
+		//se non è vuota
+		
+	   if(!parole.isEmpty()){	                                                                                //se la lista non è vuota
+		  String ultimaParola = parole.get(parole.size()-1);                                                     //ultima parola      //ok
+		  String ultimeLettere =ultimaParola.substring(ultimaParola.length()-2, ultimaParola.length());         //ultime 2 lettere   //ok
+		  String primeLettere = parolaNuova.substring(0,2);                                                    //prime 2 lettere    //ok
+		    if(ultimeLettere.equals(primeLettere)){   
+		        System.out.println(false);                   //sono legate
+			    return  false;
+		       }
+		        else{
+			             System.out.println(true);           //non sono legate
+		                 return true;
+	                 }
+	   }
+	   if(parole.isEmpty())
+	   System.out.println(false);	                //xke la devo aggiungere
+	   return false;
+	   
+	}
+	
+	public int totale(){
+		int tot=0;
+		tot= parole.size();
+		System.out.println(tot);
+		return tot;
+	}
+	
+	public void stampaParole(){
+		System.out.println(parole);
+	}
+
+}
